3acc07885d45cc8700582b6ccf534b666e78a560
limi58 <limi58@foxmail.com>
2017-07-16 13:18:24 +0800

    optimize request, remove utils

--- js/request.js
+++ js/request.js
@@ -11,29 +11,31 @@ function getHeaders () {
 }
 
 function _fetch (method, url, data = {}, opts = {}) {
+  // if exists 'http', use url
+  if (!url.match(/^http/)) {
+    url = `${config.HOST}/${url}`
+  }
   return new Promise((resolve, reject) => {
     let fetchRes
+    const fetchOpts = {
+      method,
+      body: JSON.stringify(data),
+      headers: getHeaders(),
+      ...opts,
+    }
     if (method === 'GET' || method === 'DELETE') {
-      fetchRes = fetch(url, {
-        method,
-        headers: getHeaders(),
-        ...opts
-      })
+      fetchRes = fetch(url, utils.omit(fetchOpts, 'body'))
     } else {
-      fetchRes = fetch(url, {
-        method,
-        body: JSON.stringify(data),
-        headers: getHeaders(),
-        ...opts
-      })
+      fetchRes = fetch(url, fetchOpts)
     }
     fetchRes.then(res => {
       if (res.status >= 200 && res.status < 300) {
         resolve(res.json())
       } else if (res.status === 401) {
-        storage.setItem('token', '')
         alert('请重新登录')
         // location.assign('/oauth/to-auth')
+      } else if (res.status >= 500) {
+        alert('系统繁忙，请稍后再试')
       } else {
         res.json().then(err => reject(err))
       }
@@ -41,8 +43,16 @@ function _fetch (method, url, data = {}, opts = {}) {
   })
 }
 
-export const get = (url, data = {}, setting = {}) => _fetch('GET', setQuery(url, data), '', setting)
-export const post = (url, data, setting = {}) => _fetch('POST', url, data, setting)
-export const del = (url, data = {}, setting = {}) => _fetch('DELETE', setQuery(url, data), '', setting)
-export const put = (url, data, setting = {}) => _fetch('PUT', url, data, setting)
-export const patch = (url, data, setting = {}) => _fetch('PATCH', url, data, setting)
+const get = (url, data = {}, setting = {}) => _fetch('GET', utils.setQuery(url, data), '', setting)
+const post = (url, data, setting = {}) => _fetch('POST', url, data, setting)
+const del = (url, data = {}, setting = {}) => _fetch('DELETE', utils.setQuery(url, data), '', setting)
+const put = (url, data, setting = {}) => _fetch('PUT', url, data, setting)
+const patch = (url, data, setting = {}) => _fetch('PATCH', url, data, setting)
+
+export default {
+  get,
+  post,
+  del,
+  put,
+  patch,
+}
