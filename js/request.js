0e42bde19a7ca7731ea4d976f4827f5398eb6f56
limi58 <limi58@foxmail.com>
2017-09-09 21:15:07 +0800

    add request private func

--- js/request.js
+++ js/request.js
@@ -1,12 +1,11 @@
 import 'whatwg-fetch'
 import storage from './storage'
-import { setQuery } from './utils'
 
 function getHeaders () {
   return {
     'Content-Type': 'application/json',
     Accept: 'application/json',
-    Authorization: `Bearer ${storage.getItem('token')}`
+    Authorization: `${storage.getItem('token')}`
   }
 }
 
@@ -43,9 +42,38 @@ function _fetch (method, url, data = {}, opts = {}) {
   })
 }
 
-const get = (url, data = {}, setting = {}) => _fetch('GET', utils.setQuery(url, data), '', setting)
+// 'http://www.a.com', { haha: 123, xixi: 233 }) => 'http://www.a.com?haha=123&xixi=233'
+// 'http://www.a.com?wowo=233', { haha: 123, xixi: 233 }) => 'http://www.a.com?haha=123&xixi=233&wowo=233'
+function setQuery (url, data = {}) {
+  if (Object.keys(data).length === 0) return url
+  const { data: originData, host } = parseQuery(url)
+  const query = serialize(Object.assign({}, originData, data))
+  return `${host}?${query}`
+}
+
+// { haha: 123, xixi: 233 } => haha=123&xixi=233
+function serialize (data) {
+  const keys = Object.keys(data)
+  if (keys.length === 0) return ''
+  return keys.map(key => `${key}=${data[key]}`).join('&')
+}
+
+// http://www.a.com?haha=123&xixi=233 => { host: 'http://www.a.com', data: { haha: 123, xixi: 233 } }
+function parseQuery (url) {
+  const signPosition = url.indexOf('?')
+  if (signPosition === -1) {
+    return { host: url, data: {} }
+  } else {
+    const host = url.substr(0, signPosition)
+    const query = url.substr(signPosition + 1)
+    const data = JSON.parse(`{"${query.replace(/&/g, '","').replace(/=/g, '":"')}"}`)
+    return { host, data }
+  }
+}
+
+const get = (url, data = {}, setting = {}) => _fetch('GET', setQuery(url, data), '', setting)
 const post = (url, data, setting = {}) => _fetch('POST', url, data, setting)
-const del = (url, data = {}, setting = {}) => _fetch('DELETE', utils.setQuery(url, data), '', setting)
+const del = (url, data = {}, setting = {}) => _fetch('DELETE', setQuery(url, data), '', setting)
 const put = (url, data, setting = {}) => _fetch('PUT', url, data, setting)
 const patch = (url, data, setting = {}) => _fetch('PATCH', url, data, setting)
 
